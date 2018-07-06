f19df4c1278ba80fb2f72294755fbbea314cc50c
Shannon Adair <shan@Shannons-MacBook-Pro.local>
2018-07-06 16:44:22 -0600

    cute changes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { HashRouter } from 'react-router-dom'
+import {unregister} from './registerServiceWorker'
 
 ReactDOM.render(
     <HashRouter>
         <App />
     </HashRouter>, document.getElementById('root'));;
+unregister();
