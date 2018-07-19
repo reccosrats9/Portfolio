d18430bf4fe8f21d261055829406272811a8f043
Shannon Adair <shan@Shannons-MacBook-Pro.local>
2018-07-18 18:03:48 -0600

    updated projects and carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Coverflow from 'react-coverflow';
 import { StyleRoot } from 'radium';
 
-export default () => {
+export default (props) => {
     // let style = {
     //     height: '300px',
     //     width: '500px'
@@ -27,12 +27,11 @@ export default () => {
         }
     }}
 >
-                <img src='https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=Y073vSvhXCpH1h5PcC3O9U1TlyE%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWi8LJbee-Cl9UBCLCkAjQA2d-e1EmXlF465Kt_uedgij5PlcZD5agYUbhl4lWdI' alt='Landing' //data-action="https://mypriceflights.com" target="_blank"
-                />
-                <img src='https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=8iu51wyYevzJekmfOXLqqDPOGKs%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWi8esHZfLXz8UBEKn1UjQA0fL21ETCwEI7vf4Lse9h0iMPgd8H5agYUbhl4lWdI' alt='Home' //data-action="https://mypriceflights.com" target="_blank"
-                />
-                <img src='https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=7vhIek3nKivI6qi2RPMGSWX9bdw%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjrL5SJL-PzpEATfnkHjQBneuq1ETjoEY68LN_tf9513cPnJML5agYUbhl4lWdI' alt='Match' //data-action="https://mypriceflights.com" target="_blank"
-                />
+                {props.photos.map((pic, i) => {
+                    return (
+                        <img src={pic.src} alt={pic.alt}/>
+                )    
+            })}
         </Coverflow>
     </StyleRoot>
         
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,81 +4,97 @@ import './Projects.css'
 import Carousel from './Carousel'
 
 export default () => {
+    let projects = [
+        {
+            name: 'My Price Flights',
+            live: "https://mypriceflights.com",
+            github: "https://github.com/reccosrats9/MyPriceFlights",
+            photos: [
+                {
+                    src: 'https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=Y073vSvhXCpH1h5PcC3O9U1TlyE%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWi8LJbee-Cl9UBCLCkAjQA2d-e1EmXlF465Kt_uedgij5PlcZD5agYUbhl4lWdI',
+                    alt: 'Landing'
+                },
+                {
+                    src: 'https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=8iu51wyYevzJekmfOXLqqDPOGKs%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWi8esHZfLXz8UBEKn1UjQA0fL21ETCwEI7vf4Lse9h0iMPgd8H5agYUbhl4lWdI',
+                    alt: 'Home'
+                },
+                {
+                    src: 'https://media.licdn.com/media-proxy/ext?w=800&h=800&f=n&hash=7vhIek3nKivI6qi2RPMGSWX9bdw%3D&ora=1%2CaFBCTXdkRmpGL2lvQUFBPQ%2CxAVta5g-0R6jnhodx1Ey9KGTqAGj6E5DQJHUA3L0CHH05IbfPWjrL5SJL-PzpEATfnkHjQBneuq1ETjoEY68LN_tf9513cPnJML5agYUbhl4lWdI',
+                    alt: 'Match'
+                }
+            ],
+            tech: 'REACT | NODE | CRON | STRIPE | TWILIO | AUTH0 | POSTGRES ',
+            details: [
+                'Allows users to input desired flight routes and prices, then notifies the user via text if a flight meeting specifications is found. Match dates, airlines, and prices are displayed on the user’s page.',
+                "Utilized React with particle.js and CSS styling to create a visually pleasing and intuitive user interface, allowing new users to quickly and simply use the app. Stripe payments enabled for users desiring more than 3 routes.",
+                "Developed server in Node.js, using Auth0 for authorization and connecting to Postgres database built on Heroku.",
+                'Scheduled API calls to to Skyscanner using Cron; identified matches to user specifications via javascript and notified users via text (Twilio) up to once a week per route match.',
+                "Winner of “Most Technical” award."
+            ]
+        },
+        {
+            name: 'MedTel',
+        live: 'https://medtel.devmtn-projects.com',
+        github: 'https://github.com/CARSDev/MedTel',
+            photos: [
+                {
+                    src: require('./Logos/MTsched.png'),
+                    alt: 'Scheduler'
+                },
+                {
+                    src: require('./Logos/MTdash.png'),
+                    alt: 'Patient Dashboard'
+                },
+                {
+                    src: require('./Logos/MTpt.png'),
+                    alt: 'Patient List'
+                },
+                {
+                    src: require('./Logos/MTemp.png'),
+                    alt: 'Admin View'
+                }
+            ],
+            tech: '',
+            details: [
+                'Worked with team to develop electronic medical charting system with minimal clicks and attractive page views.',
+                'Developed ability for users to schedule patient visits (custom built scheduler with Moment), track visits, conditions, allergies, medical devices, medications, family medical history, and lab/imaging results.',
+                'Built complex server and database to manage large data amounts for each patient and employee.',
+                'Developed custom middleware and account security using bcrypt.'
+            ]
+        }
+    ]
     return (
         <div className="projects" >
             <h2>Projects</h2>
-            <div className='proj' >
-                <div className="projHeader">
-                    <h3>My Price Flights</h3>
-                    <div className="projLinks" >
-                    <a href="https://mypriceflights.com" target="_blank">
-                        <h5> Live Site</h5>
-                    </a>
-                    <a href="https://github.com/reccosrats9/MyPriceFlights" target="_blank">
-                        <h5>Github Repo</h5>
-                    </a>
-                    </div>    
-                </div>
-                <Carousel />
-                <div>
+            {projects.map((val, i) => {
+                return (
+                    <div className='proj' key={i} >
+                        <div className="projHeader">
+                            <h3>{val.name}</h3>
+                            <div className="projLinks" >
+                                <a href={val.live} target="_blank">
+                                    <h5> Live Site</h5>
+                                </a>
+                                <a href={val.github} target="_blank">
+                                    <h5>Github Repo</h5>
+                                </a>
+                            </div>
+                        </div>
+                        <Carousel photos={val.photos} />
+                        <div>
 
-                    <h4>REACT | NODE | CRON | STRIPE | TWILIO | AUTH0 | POSTGRES </h4>
-                    <ul>
-                        <li>
-                            Allows users to input desired flight routes and prices, then notifies the user via text if a flight meeting specifications is found. Match dates, airlines, and prices are displayed on the user’s page.
-                        </li>
-                        <li>
-                            Utilized React with particle.js and CSS styling to create a visually pleasing and intuitive user interface, allowing new users to quickly and simply use the app. Stripe payments enabled for users desiring more than 3 routes.
-                        </li>
-                        <li>
-                            Developed server in Node.js, using Auth0 for authorization and connecting to Postgres database built on Heroku.
-                        </li>
-                        <li>
-                            Scheduled API calls to to Skyscanner using Cron; identified matches to user specifications via javascript and notified users via text (Twilio) up to once a week per route match.
-                        </li>
-                        <li>
-                            Winner of “Most Technical” award.
-                        </li>
-                    </ul>
-                </div>  
-            </div>
-{/* {SECOND PROJECT} */}
-            <div className='proj' >
-                <div className="projHeader">
-                    <h3>MedTel</h3>
-                    {/* <div className="projLinks" >
-                        <a href="https://mypriceflights.com" target="_blank">
-                            <h5> Live Site</h5>
-                        </a>
-                        <a href="https://github.com/reccosrats9/MyPriceFlights" target="_blank">
-                            <h5>Github Repo</h5>
-                        </a>
-                    </div> */}
-                </div>
-                {/* <Carousel /> */}
-                <div>
-                    <h4>Coming soon!</h4>
-
-                    {/* <h4>REACT | NODE | CRON | STRIPE | TWILIO | AUTH0 | POSTGRES </h4>
-                    <ul>
-                        <li>
-                            Allows users to input desired flight routes and prices, then notifies the user via text if a flight meeting specifications is found. Match dates, airlines, and prices are displayed on the user’s page.
-                        </li>
-                        <li>
-                            Utilized React with particle.js and CSS styling to create a visually pleasing and intuitive user interface, allowing new users to quickly and simply use the app. Stripe payments enabled for users desiring more than 3 routes.
-                        </li>
-                        <li>
-                            Developed server in Node.js, using Auth0 for authorization and connecting to Postgres database built on Heroku.
-                        </li>
-                        <li>
-                            Scheduled API calls to to Skyscanner using Cron; identified matches to user specifications via javascript and notified users via text (Twilio) up to once a week per route match.
-                        </li>
-                        <li>
-                            Winner of “Most Technical” award.
-                        </li>
-                    </ul> */}
-                </div>
-            </div>
+                            <h4>{val.tech}</h4>
+                            <ul>
+                                {val.details.map((p, i) => {
+                                    return (
+                                        <li key={i}>{p}</li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
